c8504e4e8a66e7e05153a56f29cd8bbcecf8eafa
Maurycy Blaszczak <mgblaszczak@gmail.com>
2025-05-23 00:19:52 +0200

    Hard move to uv and devenv (#21)
    * added cruft
    
    * hard move to uv and devenv
    
    * formatted

--- src/sejm_scraper/api_client.py
+++ src/sejm_scraper/api_client.py
@@ -1,5 +1,5 @@
 import httpx
-from tenacity import retry, stop_after_attempt, wait_exponential  # type: ignore
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 from sejm_scraper import schemas
 
@@ -11,37 +11,65 @@ retry_settings = {
 }
 
 
-@retry(**retry_settings)  # type: ignore
+@retry(**retry_settings)
 def get_terms(client: httpx.Client) -> list[schemas.TermSchema]:
-    result = client.get("https://api.sejm.gov.pl/sejm/term", timeout=30).raise_for_status().json()
+    result = (
+        client.get("https://api.sejm.gov.pl/sejm/term", timeout=30)
+        .raise_for_status()
+        .json()
+    )
     return [schemas.TermSchema(**item) for item in result]
 
 
-@retry(**retry_settings)  # type: ignore
-def get_sittings(client: httpx.Client, term: int) -> list[schemas.SittingSchema]:
-    result = client.get(f"https://api.sejm.gov.pl/sejm/term{term}/proceedings", timeout=30).raise_for_status().json()
+@retry(**retry_settings)
+def get_sittings(
+    client: httpx.Client, term: int
+) -> list[schemas.SittingSchema]:
+    result = (
+        client.get(
+            f"https://api.sejm.gov.pl/sejm/term{term}/proceedings", timeout=30
+        )
+        .raise_for_status()
+        .json()
+    )
     return [schemas.SittingSchema(**item) for item in result]
 
 
-@retry(**retry_settings)  # type: ignore
-def get_votings(client: httpx.Client, term: int, sitting: int) -> list[schemas.VotingSchema]:
+@retry(**retry_settings)
+def get_votings(
+    client: httpx.Client, term: int, sitting: int
+) -> list[schemas.VotingSchema]:
     result = (
-        client.get(f"https://api.sejm.gov.pl/sejm/term{term}/votings/{sitting}", timeout=30).raise_for_status().json()
+        client.get(
+            f"https://api.sejm.gov.pl/sejm/term{term}/votings/{sitting}",
+            timeout=30,
+        )
+        .raise_for_status()
+        .json()
     )
     return [schemas.VotingSchema(**item) for item in result]
 
 
-@retry(**retry_settings)  # type: ignore
-def get_votes(client: httpx.Client, term: int, sitting: int, voting: int) -> schemas.VotingWithMpVotesSchema:
+@retry(**retry_settings)
+def get_votes(
+    client: httpx.Client, term: int, sitting: int, voting: int
+) -> schemas.VotingWithMpVotesSchema:
     result = (
-        client.get(f"https://api.sejm.gov.pl/sejm/term{term}/votings/{sitting}/{voting}", timeout=30)
+        client.get(
+            f"https://api.sejm.gov.pl/sejm/term{term}/votings/{sitting}/{voting}",
+            timeout=30,
+        )
         .raise_for_status()
         .json()
     )
     return schemas.VotingWithMpVotesSchema(**result)
 
 
-@retry(**retry_settings)  # type: ignore
+@retry(**retry_settings)
 def get_mps(client: httpx.Client, term: int) -> list[schemas.MpSchema]:
-    result = client.get(f"https://api.sejm.gov.pl/sejm/term{term}/MP", timeout=30).raise_for_status().json()
+    result = (
+        client.get(f"https://api.sejm.gov.pl/sejm/term{term}/MP", timeout=30)
+        .raise_for_status()
+        .json()
+    )
     return [schemas.MpSchema(**item) for item in result]
