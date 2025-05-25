c25ebd944a6398ad9fd9c5c797d4ef3c9c6206cb
Maurycy Blaszczak <mgblaszczak@gmail.com>
2025-05-25 16:47:03 +0200

    Move to devenv v2 (#22)
    * added default interpreter, ty strategy
    
    * format, removed all comments

--- src/sejm_scraper/api_client.py
+++ src/sejm_scraper/api_client.py
@@ -7,7 +7,6 @@ retry_settings = {
     "stop": stop_after_attempt(3),
     "wait": wait_exponential(multiplier=1, min=4, max=10),
     "reraise": True,
-    # "retry": retry_if_exception_type(IOError)
 }
 
 
